e6913391532ce80c694d003582c919ab82cd1a04
jpke <j.patrick.earnest@gmail.com>
2016-12-16 17:24:55 -0500

    authentication relies on user google id instead of access token

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var http = require('http');
 var User = require('./user');
 var Questions = require('./questions');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 app.use(cors());
@@ -69,6 +70,7 @@ app.put('/question', jsonParser, passport.authenticate('bearer', { session: fals
 		}
 		var isCorrect = req.body.isCorrect
 		var dataToUpdate = data[0].questions
+		console.log("Prior Question Order: ", dataToUpdate)
 		if(dataToUpdate[0]._id == req.body._id) {
 			if(isCorrect) {
 				if(dataToUpdate[0].mValue === 1) {
@@ -91,7 +93,6 @@ app.put('/question', jsonParser, passport.authenticate('bearer', { session: fals
 				console.log("err:", err);
 			}
 			updatedData.questions = updatedQuestionOrder
-			// console.log("updatedData", updatedData.questions);
 			updatedData.save(function(err, newData) {
 				if(err) {
 					res.status(500).send(err)
@@ -110,56 +111,118 @@ app.put('/question', jsonParser, passport.authenticate('bearer', { session: fals
 	})
 })
 
-var google = require('googleapis');
-var OAuth2 = google.auth.OAuth2;
+// var google = require('googleapis');
+// var OAuth2 = google.auth.OAuth2;
 
-var oauth2Client = new OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URL
-);
+// var oauth2Client = new OAuth2(
+//   CLIENT_ID,
+//   CLIENT_SECRET,
+//   REDIRECT_URL
+// );
 
-app.get('/auth/google', function(req, res) {
-	console.log("auth/google endpoint accessed", REDIRECT_URL)
-	var url = oauth2Client.generateAuthUrl({
-	  // If you only need one scope you can pass it as string
-	  scope: 'profile'
-	});
-	res.redirect(url);
-});
+// app.get('/auth/google', function(req, res) {
+// 	console.log("auth/google endpoint accessed", REDIRECT_URL)
+// 	var url = oauth2Client.generateAuthUrl({
+// 	  // If you only need one scope you can pass it as string
+// 	  scope: 'profile'
+// 	});
+// 	res.redirect(url);
+// });
 
-app.get('/auth/google/callback', function(req, res) {
-	oauth2Client.getToken(req.query.code, function (err, tokens) {
-		console.log(FRONT_END.concat("/access_token?"+ tokens.access_token))
-	  // Now tokens contains an access_token and an optional refresh_token. Save them.
-	  	if (!err) {
-		    //oauth2Client.setCredentials(tokens);
-		    User.findOne({ token: tokens.access_token }, function (err, user) {
-					if (!err) {
-						if (!user) {
-							user = new User()
-							user.token = tokens.access_token
-							Questions.find({}, {_id: 1, mValue: 1}, function(err, data) {
-								user.questions = data
-								user.save(function(err) {
-									if (!err) {
-									console.log('user created')
-									}
-								}).then(function() {
-								res.redirect(FRONT_END.concat("/#/access_token?"+ tokens.access_token))
-								})	
-							})
-						} else {
-							// res.redirect(FRONT_END).json({"accessToken": tokens.access_token});
-							res.redirect(FRONT_END.concat("/#/access_token?"+ tokens.access_token))
+// app.get('/auth/google/callback', function(req, res) {
+// 	oauth2Client.getToken(req.query.code, function (err, tokens) {
+// 		console.log(FRONT_END.concat("/access_token?"+ tokens.access_token))
+// 	  // Now tokens contains an access_token and an optional refresh_token. Save them.
+// 	  	if (!err) {
+// 		    //oauth2Client.setCredentials(tokens);
+// 		    User.findOne({ name: profile.id }, function (err, user) {
+// 					if (!err) {
+// 						if (!user) {
+// 							console.log("new user CREATED")
+// 							user = new User()
+// 							user.name = profile.id
+// 							user.token = tokens.access_token
+// 							Questions.find({}, {_id: 1, mValue: 1}, function(err, data) {
+// 								user.questions = data
+// 								user.save(function(err) {
+// 									if (!err) {
+// 									console.log('user created')
+// 									}
+// 								}).then(function() {
+// 								res.redirect(FRONT_END.concat("/#/access_token?"+ tokens.access_token))
+// 								})	
+// 							})
+// 						} else {
+// 							user.token = tokens.access_token
+// 							user.save(function(err) {
+// 								if(!err) {
+// 									console.log('user access token updated')
+// 								}
+// 							}).then(function() {
+// 								res.redirect(FRONT_END.concat("/#/access_token?"+ tokens.access_token))
+// 								})
+// 						}
+// 					}
+// 		    });
+//   		} else {
+//   			res.status(500).json({'error': err});
+//   		}
+// 	});
+// })
+
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: REDIRECT_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOne({ name: profile.id }, function (err, user) {
+			if (!err) {
+				if (!user) {
+					console.log("new user CREATED")
+					user = new User()
+					user.name = profile.id
+					user.token = accessToken
+					Questions.find({}, {_id: 1, mValue: 1}, function(err, data) {
+						user.questions = data
+						user.save(function(err) {
+							if (!err) {
+							console.log('user created')
+							}
+						}).then(function() {
+							return cb(err, user);
+						// res.redirect(FRONT_END.concat("/#/access_token?"+ tokens.access_token))
+						})	
+					})
+				} else {
+					user.token = accessToken
+					user.save(function(err) {
+						if(!err) {
+							console.log('user access token updated')
 						}
-					}
-		    });
-  		} else {
-  			res.status(500).json({'error': err});
-  		}
-	});
-})
+					}).then(function() {
+						return cb(err, user);
+						// res.redirect(FRONT_END.concat("/#/access_token?"+ tokens.access_token))
+						})
+				}
+			}
+    });
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback/', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+  	console.log("THIS RAN!")
+  	console.log("REQ UESR: ", req.user)
+    res.redirect(FRONT_END.concat("/#/access_token?"+ req.user.token))
+  });
 
 var databaseURI = process.env.DATABASE_URI || 'mongodb://<database name>';
 mongoose.connect(databaseURI).then(function() {
