2ebf4a82dbb2ffecfc72c3e4776e5cc96b347666
jpke <j.patrick.earnest@gmail.com>
2017-03-06 15:32:01 -0500

    added demo user and refactored promises throughout

---
+++ seedQuestions.js
@@ -0,0 +1,179 @@
+var questions = [ {
+    question: 'Wermo',
+    answer: 'A stupid person',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Ee chee wa maa!',
+    answer: 'Gee whiz',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Kna Naa',
+    answer: 'Spirit tree',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Jeerota',
+    answer: 'Friend',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Kreeth',
+    answer: 'Cave',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Mookiee',
+    answer: 'Female baby Ewok',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Nuv',
+    answer: 'Love',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Ooba',
+    answer: 'Sweet',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Powa',
+    answer: 'Power',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Roda',
+    answer: 'To eat',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Sheeu',
+    answer: 'Name',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Shetai',
+    answer: 'Warrior',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Sku',
+    answer: 'Hello',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Sleesh',
+    answer: 'Berry',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Sunee',
+    answer: 'Sun',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Teeha',
+    answer: 'Thank you',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Teeket',
+    answer: 'Heart',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Sut',
+    answer: 'Soon',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Thek',
+    answer: 'Here',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Thees',
+    answer: 'Good',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'T\'hesh',
+    answer: 'Quiet',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Thuk',
+    answer: 'Rock',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Treek',
+    answer: 'Go',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Treekthin',
+    answer: 'Hourney',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Tu',
+    answer: 'The',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Weewa',
+    answer: 'House',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Yeha',
+    answer: 'Goodbye',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Yehan',
+    answer: 'Peace',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Yesh',
+    answer: 'Correct',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Drin',
+    answer: 'Sick',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Churi',
+    answer: 'Mountain',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Dutak',
+    answer: 'Arrow',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Eleeo',
+    answer: 'Never',
+    mValue: 1,
+    __v: 0 },
+  {
+    question: 'Ehda',
+    answer: 'Evil',
+    mValue: 1,
+    __v: 0 } ];
+//
+// for(question in questions) {
+// 	console.log("this ran ", question)
+// 	var newQuestion = new Questions();
+// 	newQuestion.question = questions[question].question;
+// 	newQuestion.answer = questions[question].answer;
+// 	newQuestion.mValue = 1;
+// 	newQuestion.save().then(function(question) {console.log("question saved", question)})
+// }
--- server.js
+++ server.js
@@ -40,77 +40,6 @@ passport.use(new Strategy(
 
 app.use(passport.initialize());
 
-// app.get('/*', function (req, res) {
-//    res.sendFile(path.join(__dirname, './build', 'index.html'));
-//  });
-
-app.get('/question', passport.authenticate('bearer', { session: false }), function(req, res) {
-	var accessToken = req.headers.authorization.split(' ')[1]
-	User.find({token: accessToken}, {questions: 1}, function(err, data) {
-		if (err) {
-			console.error(err)
-			return res.status(500).json('Internal Server Error')
-		}
-		Questions.find({_id: data[0].questions[0]._id}, function(err, newQuestion) {
-			if (err) {
-				console.error(err)
-				return res.status(500).json('Internal Server Error')
-			}
-			return res.status(200).json(newQuestion)
-		})
-	})
-})
-
-app.put('/question', jsonParser, passport.authenticate('bearer', { session: false }), function(req, res) {
-	var accessToken = req.headers.authorization.split(' ')[1]
-	User.find({token: accessToken}, function(err, data) {
-		if (err) {
-			console.error(err)
-			return res.status(500).json('Internal Server Error')
-		}
-		var isCorrect = req.body.isCorrect
-		var dataToUpdate = data[0].questions
-		console.log("Prior Question Order: ", dataToUpdate)
-		if(dataToUpdate[0]._id == req.body._id) {
-			if(isCorrect) {
-				if(dataToUpdate[0].mValue === 1) {
-					dataToUpdate[0].mValue = 3
-				}
-				else {
-					dataToUpdate[0].mValue = dataToUpdate[0].mValue * 2
-				}
-			}
-		}
-		else {
-			console.error(err)
-			res.status(500).json("Internal Data Error")
-		}
-		updatedQuestionOrder = dataToUpdate.slice(1, dataToUpdate[0].mValue + 1)
-		updatedQuestionOrder.push(dataToUpdate[0])
-		updatedQuestionOrder = updatedQuestionOrder.concat(dataToUpdate.slice((dataToUpdate[0].mValue + 1), dataToUpdate.length))
-		User.findById(data[0]._id, function(err, updatedData) {
-			if(err) {
-				console.log("err:", err);
-			}
-			updatedData.questions = updatedQuestionOrder
-			updatedData.save(function(err, newData) {
-				if(err) {
-					res.status(500).send(err)
-				}
-				else {
-					Questions.find({_id: newData.questions[0]._id}, function(err, question) {
-						if(err) {
-							console.error(err)
-							res.status(500).json('Question db error')
-						}
-						res.status(200).json(question)
-					})
-				}
-			})
-		})
-	})
-})
-
 // var google = require('googleapis');
 // var OAuth2 = google.auth.OAuth2;
 
@@ -150,7 +79,7 @@ app.put('/question', jsonParser, passport.authenticate('bearer', { session: fals
 // 									}
 // 								}).then(function() {
 // 								res.redirect(FRONT_END.concat("/#/access_token?"+ tokens.access_token))
-// 								})	
+// 								})
 // 							})
 // 						} else {
 // 							user.token = tokens.access_token
@@ -171,6 +100,7 @@ app.put('/question', jsonParser, passport.authenticate('bearer', { session: fals
 // })
 
 
+// authenticate through google OAuth2.0
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
@@ -182,7 +112,6 @@ passport.use(new GoogleStrategy({
     User.findOne({ name: profile.id }, function (err, user) {
 			if (!err) {
 				if (!user) {
-					console.log("new user CREATED")
 					user = new User()
 					user.name = profile.id
 					user.token = accessToken
@@ -194,8 +123,7 @@ passport.use(new GoogleStrategy({
 							}
 						}).then(function() {
 							return cb(err, user);
-						// res.redirect(FRONT_END.concat("/#/access_token?"+ tokens.access_token))
-						})	
+						})
 					})
 				} else {
 					user.token = accessToken
@@ -205,7 +133,6 @@ passport.use(new GoogleStrategy({
 						}
 					}).then(function() {
 						return cb(err, user);
-						// res.redirect(FRONT_END.concat("/#/access_token?"+ tokens.access_token))
 						})
 				}
 			}
@@ -216,15 +143,157 @@ passport.use(new GoogleStrategy({
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google/callback/', 
+app.get('/auth/google/callback/',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-  	console.log("THIS RAN!")
-  	console.log("REQ UESR: ", req.user)
-    res.redirect(FRONT_END.concat("/#/access_token?"+ req.user.token))
+		res.redirect(FRONT_END.concat("/#/access_token?"+ req.user.token))
   });
 
-var databaseURI = process.env.DATABASE_URI || 'mongodb://<database name>';
+//get quiz question through authenticated endpoint
+app.get('/question', passport.authenticate('bearer', { session: false }), function(req, res) {
+	var accessToken = req.headers.authorization.split(' ')[1]
+	User.find({token: accessToken}, {questions: 1})
+	.then(function(data) {
+		return Questions.findOne({_id: data[0].questions[0]._id}).exec();
+	})
+	.then(function(newQuestion) {
+		return res.status(200).json(newQuestion);
+	})
+	.catch(function(err) {
+		return res.status(500).json('Internal Server Error');
+	})
+})
+
+
+app.put('/question', jsonParser, passport.authenticate('bearer', { session: false }), function(req, res) {
+	var accessToken = req.headers.authorization.split(' ')[1]
+	User.findOne({token: accessToken})
+	.then(function(user) {
+		var isCorrect = req.body.isCorrect;
+		var dataToUpdate = user.questions;
+		if(dataToUpdate[0]._id == req.body._id) {
+			if(isCorrect) {
+				if(dataToUpdate[0].mValue === 1) {
+					dataToUpdate[0].mValue = 3
+				}
+				else {
+					dataToUpdate[0].mValue = dataToUpdate[0].mValue * 2
+				}
+			}
+		}
+		else {
+			throw "bad request question id";
+		}
+		updatedQuestionOrder = dataToUpdate.slice(1, dataToUpdate[0].mValue + 1)
+		updatedQuestionOrder.push(dataToUpdate[0])
+		updatedQuestionOrder = updatedQuestionOrder.concat(dataToUpdate.slice((dataToUpdate[0].mValue + 1), dataToUpdate.length))
+		user.questions = updatedQuestionOrder;
+		return user.save();
+	})
+	.then(function(user) {
+		return Questions.findOne({_id: user.questions[0]._id}).exec()
+	})
+	.then(function(nextQuestion) {
+		res.status(200).json(nextQuestion);
+	})
+	.catch(function(err) {
+		if(err === "bad request question id") return res.status(400).json("Invalid question id");
+		return res.status(500).json("Internal Server Error");
+	});
+})
+
+
+//create demo user
+//to prevent infinite demo users from being created, only 10 demo users allowed in database at one time
+app.post('/demo/question', function(req, res) {
+	//initialize new demo user
+	var user = new User()
+	user.name = "demo user"
+	user.token = "demo"
+
+	//check for 10 or more demo users, if true, remove all previous demo users
+	User.find({name: "demo user"})
+	.then(function(users) {
+		if(users.length >= 20) {
+			return users.remove().exec();
+		}
+	})
+	.then(function() {
+		return Questions.find({}, {_id: 1, mValue: 1}).exec();
+	})
+	.then(function(questions) {
+		user.questions = questions;
+		return user.save()
+	})
+	.then(function(user) {
+		res.status(201).json({id: user._id})
+	})
+	.catch(function(err) {
+		res.status(500).json("Internal Server Error");
+	})
+})
+
+//get quiz question through demo mode
+app.get('/demo/question', function(req, res) {
+	var demoUserId = req.headers.authorization.split(" ")[1];
+	User.find({_id: demoUserId}, {questions: 1})
+	.then(function(data) {
+		return Questions.findOne({_id: data[0].questions[0]._id}).exec();
+	})
+	.then(function(newQuestion) {
+		return res.status(200).json(newQuestion);
+	})
+	.catch(function(err) {
+		return res.status(500).json('Internal Server Error');
+	})
+})
+
+//update demo user's quiz question response
+app.put('/demo/question', jsonParser, function(req, res) {
+	var demoUserId = req.headers.authorization.split(' ')[1]
+	User.findById(demoUserId)
+	.then(function(user) {
+		var isCorrect = req.body.isCorrect;
+		var dataToUpdate = user.questions;
+		if(dataToUpdate[0]._id == req.body._id) {
+			if(isCorrect) {
+				if(dataToUpdate[0].mValue === 1) {
+					dataToUpdate[0].mValue = 3
+				}
+				else {
+					dataToUpdate[0].mValue = dataToUpdate[0].mValue * 2
+				}
+			}
+		}
+		else {
+			throw "bad request question id";
+		}
+		updatedQuestionOrder = dataToUpdate.slice(1, dataToUpdate[0].mValue + 1)
+		updatedQuestionOrder.push(dataToUpdate[0])
+		updatedQuestionOrder = updatedQuestionOrder.concat(dataToUpdate.slice((dataToUpdate[0].mValue + 1), dataToUpdate.length))
+		user.questions = updatedQuestionOrder;
+		return user.save();
+	})
+	.then(function(user) {
+		return Questions.findOne({_id: user.questions[0]._id}).exec()
+	})
+	.then(function(nextQuestion) {
+		res.status(200).json(nextQuestion);
+	})
+	.catch(function(err) {
+		if(err === "bad request question id") return res.status(400).json("Invalid question id");
+		return res.status(500).json("Internal Server Error");
+	});
+})
+
+
+
+
+// app.get('/*', function (req, res) {
+//    res.sendFile(path.join(__dirname, './build', 'index.html'));
+//  });
+
+var databaseURI = process.env.DATABASE_URI;
 mongoose.connect(databaseURI).then(function() {
 	var port = process.env.PORT || 3090;
 	var server = http.createServer(app);
@@ -233,5 +302,3 @@ mongoose.connect(databaseURI).then(function() {
 }).catch(function(error) {
 	console.log('Server error: ', error);
 })
-
-
